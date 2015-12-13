4a242faa3f0b6731ea49d08c6c782ec340a23726
Stephen Algeo <stephen.algeo@gmail.com>
2015-12-13 15:22:22 +0000

    Tidy up script

--- index.php
+++ index.php
@@ -20,77 +20,19 @@ error_reporting(E_ALL);
 
 		$flag = false;
 
-//		foreach($dom->getElementsByTagName('td') as $table)
-//		{
-//			echo $table->getElementsByTagName('')
-//		}
-
 		$thirdTable = $dom->getElementsByTagName('table')->item(1);
 
 		foreach($thirdTable->getElementsByTagName('tr') as $tr)
 		{
-//			echo 'row';
-//echo $tds->length;
 			$tds = $tr->getElementsByTagName('td'); // get the columns in this row
 			if($tds->length >= 4)
 			{
-				// check if B and D are found in column 2 and 4
-//				if(trim($tds->item(1)->nodeValue) == 'B' && trim($tds->item(3)->nodeValue) == 'D')
-//				{
-					// found B and D in the second and fourth columns
-					// echo out each column value
-//					echo $tds->item(0)->nodeValue; // A
-//					echo $tds->item(1)->nodeValue; // B
-
 				$array[$locale][] = trim($tds->item(1)->nodeValue);
-//					echo $tds->item(2)->nodeValue; // C
-//					echo $tds->item(3)->nodeValue; // D
-//					break; // don't check any further rows
-//				}
 			}
 		}
-
-
-
-//		exit;
-//        foreach($dom->getElementsByTagName('td') as $node)
-//        {
-//            $text = trim($node->nodeValue);
-//            $text = trim($text);
-//            if(strlen($text)<=2) continue;
-//            if (substr($text, 0, 1)=='-') continue;
-////            if (strstr($text, '-') AND !strstr($text, 'Blu-ray')) continue;
-//            if (strstr($text, 'Documentation')) continue;
-//            if (strstr($text, 'PercentageOff')) continue;
-//            if (strstr($text, 'songtitle')) continue;
-//            if (strstr($text, 'Amazon Web Services')) continue;
-//            if (strstr($text, 'AWS Documentation')) continue;
-//            if (strstr($text, 'Document Conventions')) continue;
-//            if (strstr($text, 'Terms of Use')) continue;
-//            if (strstr($text, 'Did this page help')) continue;
-//            if (strstr($text, 'Previous')) continue;
-//            if (strstr($text, '.pdf')) continue;
-//            if (strstr($text, '<img')) continue;
-//            if (strstr($text, '<p')) continue;
-//            if (strstr($text, 'padding')) continue;
-//            if (strstr($text, 'salesranktitlerank')) continue;
-//            if (strstr($text, 'Search index')) continue;
-//            if (strstr($text, 'Availability')) continue;
-//            if (strstr($text, '-price')) continue;
-//            if (strstr($text, 'Â')) continue;
-//            if (trim($text)=='') continue;
-//            if ($text==' ') continue;
-//            if (is_numeric($text)) continue;
-//
-//			if ($flag){
-//				$array[$locale][] = trim($text);
-//			}
-//
-//			$flag = !$flag;
-//        }
     }
 
-    $sql =  '-- Update Search Indexes at http://docs.freshdevelopment.org/searchindexes/ <br>';
+    $sql =  '-- Script to refresh at https://github.com/sjalgeo/amazon-search-indexes <br>';
 
     $sql .=  'TRUNCATE TABLE searchindexes;<br>';
     $sql .= 'INSERT INTO `searchindexes` (`locale`, `searchindex`) VALUES <br>';
