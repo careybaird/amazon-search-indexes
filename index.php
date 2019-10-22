d9686ed12b907cc440b67fc9bde13a42874a786e
Vivek Anand Tiwari <Vivek-Hsoft@users.noreply.github.com>
2019-10-22 14:33:12 +0530

    Updated for PAAPI5

--- index.php
+++ index.php
@@ -6,13 +6,12 @@ echo '<pre>';
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-    $regions = array('CA', 'CN', 'DE', 'ES', 'FR', 'IN', 'IT', 'JP', 'UK', 'US', 'BR');
-//    $regions = array('DE');
+    //$regions = array('CA', 'CN', 'DE', 'ES', 'FR', 'IN', 'IT', 'JP', 'UK', 'US', 'BR');
+    $regions = array('AU'=>'australia','BR'=>'brazil','CA'=>'canada','FR'=>'france','DE'=>'germany','IN'=>'india','IT'=>'italy','JP'=>'japan','MX'=>'mexico','ES'=>'spain','TR'=>'turkey','AE'=>'united-arab-emirates','UK'=>'united-kingdom','US'=>'united-states');
+    //$regions = array('AU'=>'australia');
 
-    foreach ($regions as $locale) {
-//        $url = 'http://docs.aws.amazon.com/AWSECommerceService/latest/DG/'.$locale.'SearchIndexParamForItemsearch.html';
-        $url = 'http://docs.aws.amazon.com/AWSECommerceService/latest/DG/Locale'.$locale.'.html';
-//		echo $url;
+    foreach ($regions as $localeKey=>$localePage) {
+        $url = 'https://webservices.amazon.com/paapi5/documentation/locale-reference/'.$localePage.'.html';
         $string = file_get_contents($url);
 
         $dom = new DOMDocument;
@@ -20,18 +19,17 @@ error_reporting(E_ALL);
 
 		$flag = false;
 
-		$thirdTable = $dom->getElementsByTagName('table')->item(1);
+		$thirdTable = $dom->getElementsByTagName('table')->item(2);
 
 		foreach($thirdTable->getElementsByTagName('tr') as $tr)
 		{
-			$tds = $tr->getElementsByTagName('td'); // get the columns in this row
-			if($tds->length >= 4)
+            $tds = $tr->getElementsByTagName('td');
+            if($tds->item(0))
 			{
-				$array[$locale][] = trim($tds->item(1)->nodeValue);
+				$array[$localeKey][] = trim($tds->item(0)->nodeValue);
 			}
 		}
     }
-
     $sql =  '-- Script to refresh at https://github.com/sjalgeo/amazon-search-indexes <br>';
 
     $sql .=  'TRUNCATE TABLE searchindexes;<br>';
